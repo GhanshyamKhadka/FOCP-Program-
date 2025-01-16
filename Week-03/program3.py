5051b8473e36347dbed4db58090c882fef737516
GhanshyamKhadka <160210230+GhanshyamKhadka@users.noreply.github.com>
2025-01-16 15:13:17 +0545

    Delete Week-03 directory

--- Week-03/program3.py
+++
@@ -1,9 +0,0 @@
-'''3.Modify your previous program so that the password must be between 8 and 12
- characters (inclusive) long.'''
-
-password= input("Enter a password: ")
-if len(password)<8 or len(password)>12:
-    print("Error: Password must be between 8 and 12 characters long.")
-
-else:
-    print("Password set.")    
